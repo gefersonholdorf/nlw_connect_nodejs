84cae2dbdd56b2a1bc8ca952ed2b3fab9ac94fce
gefersonholdorf <gefersonholdorf88@gmail.com>
2025-02-18 23:03:21 -0300

    Database integration and first features

---
+++ src/functions/access-invite-link.ts
@@ -0,0 +1,11 @@
+import { redis } from '../redis/client'
+
+export interface AccessInviteLinkParams {
+  subscriberId: string
+}
+
+export async function accessInviteLink({
+  subscriberId,
+}: AccessInviteLinkParams) {
+  await redis.hincrby('referral:access-count', subscriberId, 1)
+}
